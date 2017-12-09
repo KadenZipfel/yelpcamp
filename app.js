09357713b790cedfc7f4b61d65244aca27ac721f
Kaden Zipfel <h.fan98@hotmail.com>
2017-12-09 22:26:53 +0000

    Update dburl

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
     
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://kadenzipfel:HabHabHabgm98@ds135486.mlab.com:35486/yelpcamp_kzipfel");
+console.log(process.env.DATABASEURL);
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://kadenzipfel:HabHabHabgm98@ds135486.mlab.com:35486/yelpcamp_kzipfel");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
